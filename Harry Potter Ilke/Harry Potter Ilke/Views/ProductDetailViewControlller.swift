3863403ffd02f32c9bab3abfc444bae323ccb3a3
Ilke Yucel <ilke.yucel@morhipo.com>
2021-07-25 00:12:05 +0300

    - Productlist detail added.

---
+++ Harry Potter Ilke/Harry Potter Ilke/Views/ProductDetailViewControlller.swift
@@ -0,0 +1,13 @@
+//
+//  ProductDetailViewControlller.swift
+//  Harry Potter Ilke
+//
+//  Created by Ilke Yucel on 24.07.2021.
+//
+
+import UIKit
+import Combine
+
+class ProductDetailViewControlller: UIViewController {
+    
+}
